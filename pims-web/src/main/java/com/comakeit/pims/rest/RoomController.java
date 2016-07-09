59d11ab038e4fef945dd5df4cc3b4700c642a1bb
unknown <baddanki@E1076F1L.lsz.ads.valuelabs.net>
2016-07-10 00:51:51 +0530

    Enabled first resetful service

--- pims-web/src/main/java/com/comakeit/pims/rest/RoomController.java
+++ pims-web/src/main/java/com/comakeit/pims/rest/RoomController.java
@@ -1,8 +1,14 @@
 package com.comakeit.pims.rest;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.comakeit.pims.data.dto.RoomDTO;
 import com.comakeit.pims.rest.base.AbstractBaseController;
 
+@RestController
+@RequestMapping(value = RoomController.BASE_URL)
 public class RoomController extends AbstractBaseController<RoomDTO> {
 
+	protected final static String BASE_URL = "/room";
 }
--- pims-web/src/main/java/com/comakeit/pims/rest/base/AbstractBaseController.java
+++ pims-web/src/main/java/com/comakeit/pims/rest/base/AbstractBaseController.java
@@ -6,7 +6,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.comakeit.pims.data.dto.base.AbstractBaseDTO;
@@ -20,10 +19,8 @@ public abstract class AbstractBaseController<T extends AbstractBaseDTO> extends 
 
 	@RequestMapping(value = "", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseStatus(HttpStatus.OK)
-	public Object list(@RequestParam(value = "containerType", defaultValue = "workspace") final String containerType) {
-		Object returnObject = null;
-
-		return returnObject;
+	public String list() {
+		return "It is working";
 	}
 
 }
